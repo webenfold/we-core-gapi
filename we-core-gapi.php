a2387df41d7c8dffd1e00e44dab504776e139707
Gaurav Kumar <gk@webenfold.com>
2018-05-21 11:55:20 +0530

    V 1.0

---
+++ we-core-gapi.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name:  WE CORE GOOGLE API
+Plugin URI:   https://www.webenfold.com
+description:  Google authenticator - Part of WebEnfold WE-Core Engine plugins. 
+Version:      1.0
+Author:       WebEnfold
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
